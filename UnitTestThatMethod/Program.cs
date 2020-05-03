7e17d370e5a935eb777edf0ec4f43469de3fceac
rmanshouri <reza.manshouri@factset.com>
2020-05-03 11:48:23 -0400

    First commit

---
+++ UnitTestThatMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestThatMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
